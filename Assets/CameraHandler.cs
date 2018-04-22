2c4cdf9a8410f2a91c03ad1b34940b68967521fb
panagioths-k <panos_kotsikoris@hotmail.com>
2018-04-22 20:45:37 +0300

    init

---
+++ Assets/CameraHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour {
+
+    Camera mainCamera;                //Main Camera of the scene
+    public float forwardZspeed;
+    Vector3 speedVector;
+
+    // Use this for initialization
+    void Start () {
+
+     //   Debug.Log("Started");
+        mainCamera = Camera.main;     //get Main Camera
+        speedVector = new Vector3(0f, 0f, forwardZspeed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+     //   Debug.Log("Updating...");
+        mainCamera.transform.position += speedVector;
+
+    }
+}
---
+++ Assets/HandleHero.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleHero : MonoBehaviour {
+
+    public GameObject hero;
+    public Vector3 herosDistanceFromCamera;
+    public float horizontalHeroLeap;
+
+    public float smoothTime = 0.3F;
+    public Vector3 velocity = Vector3.zero;
+
+    private int direction;                          //defines whether the hero moves left (1) or right (-1)
+
+    // Use this for initialization
+    void Start() {
+
+        GameObject c = gameObject;                  //generate hero in front of camera
+        hero = Instantiate(hero, gameObject.transform.transform.position + herosDistanceFromCamera, gameObject.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1) || (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Stationary)) )
+        {
+            direction = 1;
+            if (Input.mousePosition.x < Screen.width / 2)
+            {
+                direction = -1;
+            }
+            hero.transform.position = Vector3.SmoothDamp(hero.transform.position,
+                    new Vector3(hero.transform.position.x + ( horizontalHeroLeap * direction), hero.transform.position.y, hero.transform.position.z),
+                    ref velocity, smoothTime);
+           
+        }
+    }
+}
